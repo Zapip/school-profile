901eb2787609541cf92f6b9f46b318772d3b5937
GayusTarihoran1 <gayusknight2@gmail.com>
2025-04-16 10:22:02 +0700

    merge fasilitas

--- src/components/shared/profile.jsx
+++ src/components/shared/profile.jsx
@@ -3,7 +3,7 @@ import VideoBrowserFrame from "../atoms/VideoCard";
 
 const Profile = () => {
   return (
-    <section className="bg-[url('/bgprofile.png')] bg-cover justify-center bg-center h-full w-full ">
+    <section className="bg-[url('/bgprofile.png')] bg-cover bg-center h-full w-full ">
       <h1 className="flex text-3xl justify-center md:text-6xl font-black text-gray pt-15">
         Bersama SMAN X, Raih Pendidikan Terbaik
       </h1>
@@ -14,30 +14,7 @@ const Profile = () => {
       </h3>
 
       <div className="w-full h-220 aspect-video flex justify-center">
-        <div className="w-full max-w-7xl mx-auto px-4 pb-4">
-          {/* Frame header seperti browser tab */}
-          <div className="bg-white rounded-t-xl px-4 py-2 flex items-center justify-between">
-            <div className="flex gap-2">
-              <span className="w-3 h-3 bg-red-500 rounded-full"></span>
-              <span className="w-3 h-3 bg-yellow-500 rounded-full"></span>
-              <span className="w-3 h-3 bg-green-500 rounded-full"></span>
-            </div>
-            <span className="text-sm text-gray-600">uteach.com</span>
-            <div className="w-16" /> {/* Spacer */}
-          </div>
-
-          {/* Iframe Container */}
-          <div className="aspect-video w-full bg-black rounded-b-xl overflow-hidden shadow-lg">
-            <iframe
-              className="w-full h-full"
-              src="https://www.youtube.com/embed/CGr98yMNA5E?si=isqUCiNw0ndDUKaP"
-              title="YouTube Video"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </div>
+        <VideoBrowserFrame videoUrl="https://www.youtube.com/embed/CGr98yMNA5E?si=isqUCiNw0ndDUKaP" />
       </div>
     </section>
   );
