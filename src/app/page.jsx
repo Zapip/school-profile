d893693d10c406e3c03e3dff88fa5a8c05c0a94c
GayusTarihoran1 <gayusknight2@gmail.com>
2025-04-16 11:27:06 +0700

    Stat Final

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,8 @@ import Ppdb from "@/components/shared/ppdb";
 import Testimoni from "@/components/shared/testimoni";
 import Profile from "@/components/shared/profile";
 import Fasilitas from "@/components/shared/fasilitas";
+import Kegiatan from "@/components/shared/kegiatan";
+import Stat from "@/components/shared/stat";
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
       <section id="hero"> <Hero /> </section>
       <section id="profile"> <Profile /> </section>
       <section id="fasilitas"> <Fasilitas /></section>
+      <section id="kegiatan"><Kegiatan /></section>
+      <section id="stat"><Stat /></section>
       <section id="testimoni"> <Testimoni /> </section>
       <section id="blog"> <Blog /> </section>
       <section id="ppdb"> <Ppdb /> </section>
