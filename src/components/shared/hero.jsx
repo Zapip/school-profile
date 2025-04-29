1eb0792aa05a3f8a76567c771a49dafb71dba919
Zapip <zafif3165@gmail.com>
2025-04-29 15:51:27 +0700

    feat(view): add responsive and several image on page hero section

--- src/components/shared/hero.jsx
+++ src/components/shared/hero.jsx
@@ -1,53 +1,84 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import {PlayCircleIcon} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import { PlayCircleIcon } from "@heroicons/react/24/outline";
 
-export default function Hero() {
+const Hero = () => {
   return (
-    <section className="w-full px-5 md:px-20 py-23">
-      <div className="mx-auto flex flex-col md:flex-row justify-between items-center w-full gap-0">
+    <article className="w-full h-dvh p-4 pt-24 md:p-20 md:pt-10">
+      <section className="flex flex-row justify-between items-center w-full gap-0">
         {/* LEFT: Text */}
-        <div className="text-center md:text-left w-1/2 pr-20 ">
-          <h1 className="text-5xl md:text-[78px] font-black text-gray-900 leading-tight">
-            Pendidikan <br /> Berkualitas <br />untuk Generasi Masa Depan
+        <section className="text-center md:text-left md:w-1/2">
+          <h1 className="text-2xl md:text-5xl  font-black text-gray leading-tight">
+            Pendidikan Berkualitas untuk Generasi Masa Depan
           </h1>
-          <p className="mt-6 text-gray-700 text-[28px]">
-            Lingkungan belajar yang kondusif dan fasilitas modern untuk mendukung perkembangan potensi siswa secara optimal.
+          <p className="mt-6 text-gray-700 text-xl md:text-2xl">
+            Lingkungan belajar yang kondusif dan fasilitas modern untuk
+            mendukung perkembangan potensi siswa secara optimal.
           </p>
-          <div className="mt-8 flex flex-wrap gap-5 justify-center md:justify-start">
-            <button className="bg-orange-600 hover:bg-orange-700 text-white text-[28px] font-semibold px-17 py-5 rounded-2xl shadow w-auto h-auto ">
+          <section className="mt-8 flex flex-col md:flex-row gap-5 justify-center items-center md:justify-start">
+            <a
+              href="#"
+              className="bg-orange-600 hover:bg-orange-700 text-white text-2xl font-semibold px-6 py-4 rounded-2xl shadow size-fit"
+            >
               Daftar Sekarang
-            </button>
-            <button className="flex items-center gap-3 text-blue-600 font-medium text-[26px]">
-                <span className="material-icons">
-                <PlayCircleIcon className='w-8 h-8'/>
-                </span> 
-                Profil Sekolah
-            </button>
-          </div>
-          <div className="mt-8 text-sm text-gray-400">
-            <Image
-                src="/Press.png"
-                alt="Lembaga"
-                width={300}
-                height={20}
-                className='w-full h-auto pt-20'
-            />
-          </div>
-        </div>
+            </a>
+            <a
+              href="#"
+              className="flex items-center gap-3 text-blue-600 font-medium text-2xl"
+            >
+              <span className="material-icons">
+                <PlayCircleIcon className="w-8 h-8" />
+              </span>
+              Profil Sekolah
+            </a>
+          </section>
+          <section className="mt-8 text-sm text-gray-400 flex items-center justify-center md:justify-start gap-4 size-full">
+            <p>
+              Di percaya oleh <br />
+              Lembaga Pendidikan
+            </p>
+            <section className="flex gap-2 grayscale">
+              <span className="relative size-8">
+                <Image
+                  src="/kemendikbud.png"
+                  alt="kemendikbud"
+                  fill
+                  className="object-contain"
+                />
+              </span>
+              <span className="relative size-8">
+                <Image
+                  src="/pgri.png"
+                  alt="kemendikbud"
+                  fill
+                  className="object-contain"
+                />
+              </span>
+              <span className="relative size-8">
+                <Image
+                  src="/osis.png"
+                  alt="osis"
+                  fill
+                  className="object-contain"
+                />
+              </span>
+            </section>
+          </section>
+        </section>
 
         {/* RIGHT: Image */}
-        <div className="md:mt-0">
+        <section className="md:mt-0 hidden md:block relative size-[500px]">
           <Image
             src="/boy-student.png"
             alt="Siswa"
-            width={400}
-            height={550}
-            className="w-[750px] h-auto"
+            fill
+            className="object-contain"
           />
-        </div>
-      </div>
-    </section>
+        </section>
+      </section>
+    </article>
   );
-}
+};
+
+export default Hero;
