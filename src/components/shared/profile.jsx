dc30f86f839360fc6247abaf031c3f7444d4a9ca
Zapip <zafif3165@gmail.com>
2025-04-29 16:04:02 +0700

    feat(profile): added responsive

--- src/components/shared/profile.jsx
+++ src/components/shared/profile.jsx
@@ -1,22 +1,23 @@
-import Image from "next/image";
 import VideoBrowserFrame from "../atoms/VideoCard";
 
 const Profile = () => {
   return (
-    <section className="bg-[url('/bgprofile.png')] bg-cover bg-center h-full w-full ">
-      <h1 className="flex text-3xl justify-center md:text-6xl font-black text-gray pt-15">
+    <article className="bg-[url('/bgprofile.png')] bg-cover bg-center h-fit w-full p-4 md:p-20 py-8">
+      <h1 className="flex text-3xl justify-center md:text-6xl font-black text-gray text-center">
         Bersama SMAN X, Raih Pendidikan Terbaik
       </h1>
 
-      <h3 className="px-20 text-center text-xl py-15 text-gray">
-      Dengan fasilitas modern, tenaga pendidik yang kompeten, dan kurikulum yang inovatif, SMA X siap mengantarkan siswa menuju masa depan yang gemilang. Bergabunglah bersama kami dan rasakan pengalaman pendidikan yang tak terlupakan.
-
+      <h3 className="text-center text-sm md:text-xl text-gray my-12 mt-4">
+        Dengan fasilitas modern, tenaga pendidik yang kompeten, dan kurikulum
+        yang inovatif, SMA X siap mengantarkan siswa menuju masa depan yang
+        gemilang. Bergabunglah bersama kami dan rasakan pengalaman pendidikan
+        yang tak terlupakan.
       </h3>
 
-      <div className="w-full h-220 aspect-video flex justify-center">
+      <section className="w-full h-full aspect-video flex justify-center">
         <VideoBrowserFrame videoUrl="https://www.youtube.com/embed/CGr98yMNA5E?si=isqUCiNw0ndDUKaP" />
-      </div>
-    </section>
+      </section>
+    </article>
   );
 };
 
